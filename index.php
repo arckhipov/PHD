c4d2ed72f47b5ce1a37b3893d07e55c71d3f4c95
vitaliy.arkhipov <odeshop@ya.ru>
2018-02-13 21:10:14 +0300

    First commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Назначениие ПХД</title>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Belleza" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12" >
+            <a class="btn btn-warning" href="duty2.php?month=January">Январь</a>
+            <a class="btn btn-warning" href="duty2.php?month=February">Февраль</a>
+            <a class="btn btn-warning" href="duty2.php?month=March">Март</a>
+            <a class="btn btn-warning" href="duty2.php?month=April">Апрель</a>
+            <a class="btn btn-warning" href="duty2.php?month=May">Май</a>
+            <a class="btn btn-warning" href="duty2.php?month=June">Июнь</a>
+            <a class="btn btn-warning" href="duty2.php?month=July">Июль</a>
+            <a class="btn btn-warning" href="duty2.php?month=August">Август</a>
+            <a class="btn btn-warning" href="duty2.php?month=September">Сентябрь</a>
+            <a class="btn btn-warning" href="duty2.php?month=October">Октябрь</a>
+            <a class="btn btn-warning" href="duty2.php?month=November">Ноябрь</a>
+            <a class="btn btn-warning" href="duty2.php?month=December">Декабрь</a>
+        </div>
+    </div>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
